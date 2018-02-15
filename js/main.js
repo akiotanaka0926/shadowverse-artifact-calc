a3f2326443b3b6aa656bd6bf7c1bc2bd541b611d
atanaka <atanaka@atled.jp>
2018-02-15 14:34:32 +0900

    add calc probability

--- js/main.js
+++ js/main.js
@@ -5,67 +5,79 @@ $(function() {
   var radiant_count = 0;
   var deck_count = 37;
 
+  var probability = function() {
+    var analyze = (analyze_count/deck_count*100).toFixed(3);
+    $('#analyze_probability').text(analyze);
+    var ancient = (ancient_count/deck_count*100).toFixed(3);
+    $('#ancient_probability').text(ancient);
+    var mistic = (mistic_count/deck_count*100).toFixed(3);
+    $('#mistic_probability').text(mistic);
+    var radiant = (radiant_count/deck_count*100).toFixed(3);
+    $('#radiant_probability').text(radiant);
+  }
+
   var changeDeckCount = function (modifyNumber) {
     if ($('.sync_deck:checked').val() != 1) return;
     deck_count += modifyNumber;
     $('#deck_counter').text(deck_count);
     $('#deck_num').val(deck_count);
+    probability();
   };
 
   $('#analyze_plus_one').click(function() {
-    changeDeckCount(1);
     $('#analyze_counter').text(++analyze_count);
+    changeDeckCount(1);
   });
   $('#analyze_plus_two').click(function() {
-    changeDeckCount(2);
     analyze_count += 2;
     $('#analyze_counter').text(analyze_count);
+    changeDeckCount(2);
   });
   $('#analyze_minus').click(function() {
-    changeDeckCount(-1);
     $('#analyze_counter').text(--analyze_count);
+    changeDeckCount(-1);
   });
 
   $('#ancient_plus_one').click(function() {
-    changeDeckCount(1);
     $('#ancient_counter').text(++ancient_count);
+    changeDeckCount(1);
   });
   $('#ancient_plus_two').click(function() {
-    changeDeckCount(2);
     ancient_count += 2;
     $('#ancient_counter').text(ancient_count);
+    changeDeckCount(2);
   });
   $('#ancient_minus').click(function() {
-    changeDeckCount(-1);
     $('#ancient_counter').text(--ancient_count);
+    changeDeckCount(-1);
   });
 
   $('#mistic_plus_one').click(function() {
-    changeDeckCount(1);
     $('#mistic_counter').text(++mistic_count);
+    changeDeckCount(1);
   });
   $('#mistic_plus_two').click(function() {
-    changeDeckCount(2);
     mistic_count += 2;
     $('#mistic_counter').text(mistic_count);
+    changeDeckCount(2);
   });
   $('#mistic_minus').click(function() {
-    changeDeckCount(-1);
     $('#mistic_counter').text(--mistic_count);
+    changeDeckCount(-1);
   });
 
   $('#radiant_plus_one').click(function() {
-    changeDeckCount(1);
     $('#radiant_counter').text(++radiant_count);
+    changeDeckCount(1);
   });
   $('#radiant_plus_two').click(function() {
-    changeDeckCount(2);
     radiant_count += 2;
     $('#radiant_counter').text(radiant_count);
+    changeDeckCount(2);
   });
   $('#radiant_minus').click(function() {
-    changeDeckCount(-1);
     $('#radiant_counter').text(--radiant_count);
+    changeDeckCount(-1);
   });
 
   $('#deck_plus_two').click(function() {
@@ -88,6 +100,7 @@ $(function() {
     $('#ancient_counter').text(ancient_count);
     $('#mistic_counter').text(mistic_count);
     $('#radiant_counter').text(radiant_count);
+    probability();
   });
 
   $('#reset_deck').click(function() {
@@ -95,11 +108,13 @@ $(function() {
 
     $('#deck_counter').text(deck_count);
     $('#deck_num').val(deck_count);
+    probability();
   });
 
   $('#deck_num').change(function() {
     var ch = $('#deck_num').val();
     $('#deck_counter').text(ch);
+    probability();
   });
 });
 
